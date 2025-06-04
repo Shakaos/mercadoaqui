2511c3031429a0c7d8297c8eb52ba22398d2872d
Shakaos <132714947+Shakaos@users.noreply.github.com>
2025-06-04 15:53:35 -0400

    Update usuarios.module.ts

--- src/modules/usuarios/usuarios.module.ts
+++ src/modules/usuarios/usuarios.module.ts
@@ -5,6 +5,8 @@ import { UsuariosService } from './usuarios.service';
 import { Usuario } from './usuario.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { RolesGuard } from './roles.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -18,6 +20,13 @@ import { JwtStrategy } from './jwt.strategy';
     }),
   ],
   controllers: [UsuariosController],
-  providers: [UsuariosService, JwtStrategy],
+  providers: [
+    UsuariosService,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class UsuariosModule {}
