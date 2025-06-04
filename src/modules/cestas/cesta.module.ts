009300130a04c4a1118c09aa90eab040044d50a3
Shakaos <132714947+Shakaos@users.noreply.github.com>
2025-06-03 23:30:45 -0400

    Update cesta.module.ts

--- src/modules/cestas/cesta.module.ts
+++ src/modules/cestas/cesta.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CestaBasica } from './cesta.entity';
 import { CestaController } from './cesta.controller';
 import { Mercado } from '../mercados/mercado.entity';
+import { CestaProduto } from './cesta_produto.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CestaBasica, Mercado])],
+  imports: [TypeOrmModule.forFeature([CestaBasica, Mercado, CestaProduto])],
   controllers: [CestaController],
 })
 export class CestaModule {} 
