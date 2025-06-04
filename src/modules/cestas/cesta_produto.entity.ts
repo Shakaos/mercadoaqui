ce7a4caf9b31faefd175b41f2de87a4fc7b411f4
Shakaos <132714947+Shakaos@users.noreply.github.com>
2025-06-03 23:31:50 -0400

    Add files via upload

---
+++ src/modules/cestas/cesta_produto.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, Column } from 'typeorm';
+import { CestaBasica } from './cesta.entity';
+import { Produto } from '../produtos/produto.entity';
+
+@Entity('cesta_produto')
+export class CestaProduto {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => CestaBasica, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'cesta_id' })
+  cesta: CestaBasica;
+
+  @ManyToOne(() => Produto, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'produto_id' })
+  produto: Produto;
+
+  // Opcional: quantidade do produto na cesta
+  @Column({ type: 'int', default: 1 })
+  quantidade: number;
+} 
