c223cc93d27ec82ac46507841183da8b7e13042b
Shakaos <132714947+Shakaos@users.noreply.github.com>
2025-06-03 21:09:23 -0400

    Update lista.module.ts

--- src/modules/listas/lista.module.ts
+++ src/modules/listas/lista.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lista } from './lista.entity';
 import { ListaController } from './lista.controller';
 import { Usuario } from '../usuarios/usuario.entity';
+import { ItemLista } from './item-lista.entity';
+import { Produto } from '../produtos/produto.entity';
+import { Preco } from '../precos/preco.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Lista, Usuario])],
+  imports: [TypeOrmModule.forFeature([Lista, ItemLista, Usuario, Produto, Preco])],
   controllers: [ListaController],
 })
 export class ListaModule {} 
