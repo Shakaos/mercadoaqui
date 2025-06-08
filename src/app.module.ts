e185a54a7a47984cf79e34a317d14704866ae59d
Shakaos <132714947+Shakaos@users.noreply.github.com>
2025-06-08 16:06:04 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,7 +34,7 @@ import { ItemLista } from './modules/listas/item-lista.entity';
       database: process.env.DB_NAME,
       entities: [Usuario, Produto, Preco, Mercado, CestaBasica, CestaProduto, Lista, ItemLista],
       logging: true,
-      synchronize: process.env.NODE_ENV !== 'production',
+      synchronize: true, // ATIVAR TEMPORARIAMENTE para criar a tabela
     }),
 
     // Módulo JWT global
